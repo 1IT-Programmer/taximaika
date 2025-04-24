2cf949aaa49eb0af4dd3b0cbb6009d311a9f8f39
IT Programmer <40290724+1IT-Programmer@users.noreply.github.com>
2025-04-24 16:21:37 +0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 # Базовые настройки
-SECRET_KEY = "your_super_secret_key"
+SECRET_KEY = "9fa18e1ef5fac770d5f5b2e0ef1f5c493baea30c799d8399878e7b757282d5ef"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
